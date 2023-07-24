70233defbdc82d4c8e313aec592c2a90d3f0f7ea
Morinobu Kuzunuki <morinobukuzunuki@gmail.com>
2023-07-24 20:07:40 +0900

    improve test code readability.

--- tests/QueueTests.elm
+++ tests/QueueTests.elm
@@ -1,96 +1,155 @@
 module QueueTests exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, bool, int, listOfLength, pair, string)
-import Queue exposing (..)
+import Fuzz exposing (Fuzzer)
+import List.Extra
+import Queue exposing (Queue)
 import Test exposing (..)
 
 
 suite : Test
 suite =
     describe "The Queue module"
-        [ test "list conversion is working collectly."
-            (\_ ->
+        [ Test.fuzz operationsFuzzer
+            "Checking random read-and-write."
+            (\( initialList, operations ) ->
                 let
-                    inputList =
-                        [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ]
+                    queueOutput =
+                        queueHelp operations
+                            { queue = Queue.fromListFIFO initialList
+                            , youngestSnapShots = []
+                            , oldestSnapShots = []
+                            }
+
+                    listOutput =
+                        listHelp operations
+                            { queue = initialList
+                            , youngestSnapShots = []
+                            , oldestSnapShots = []
+                            }
+
+                    revQueueOutput =
+                        queueHelp operations
+                            { queue = Queue.fromListLIFO initialList
+                            , youngestSnapShots = []
+                            , oldestSnapShots = []
+                            }
+
+                    revListOutput =
+                        listHelp operations
+                            { queue = List.reverse initialList
+                            , youngestSnapShots = []
+                            , oldestSnapShots = []
+                            }
                 in
-                inputList
-                    |> Queue.fromListLIFO
-                    |> Queue.toListFIFO
-                    |> Expect.equal inputList
-            )
-        , Test.fuzz (Fuzz.listOfLength 10 (pair bool int))
-            "enqueue gives the same result as List."
-            (\l ->
-                let
-                    queue =
-                        List.foldl
-                            (\( b, i ) q ->
-                                if b then
-                                    enqueue i q
-
-                                else
-                                    q
-                            )
-                            empty
-                            l
-
-                    lis =
-                        List.foldl
-                            (\( b, i ) li ->
-                                if b then
-                                    i :: li
-
-                                else
-                                    li
-                            )
-                            []
-                            l
-                in
-                queue
-                    --|> Debug.log "queue"
-                    |> toListLIFO
-                    |> Expect.equal lis
-            )
-        , Test.fuzz (Fuzz.listOfLength 1000 (pair bool int))
-            "Random read and write gives the same result as List."
-            (\l ->
-                let
-                    start =
-                        List.range 1 10
-
-                    queueHelp li q outL =
-                        case li of
-                            [] ->
-                                ( toListLIFO q, outL, Debug.log "queue length" (length q) )
-
-                            ( b, i ) :: rest ->
-                                if b then
-                                    queueHelp rest (enqueue i q) (head q :: outL)
-
-                                else
-                                    queueHelp rest (dequeue q) (head q :: outL)
-
-                    queue =
-                        queueHelp l (fromListFIFO start) []
-
-                    listHelp rst soFar out =
-                        case rst of
-                            [] ->
-                                ( soFar, out, List.length soFar )
-
-                            ( b, i ) :: rest ->
-                                if b then
-                                    listHelp rest (i :: soFar) (List.head (List.drop (List.length soFar - 1) soFar) :: out)
-
-                                else
-                                    listHelp rest (List.take (List.length soFar - 1) soFar) (List.head (List.drop (List.length soFar - 1) soFar) :: out)
-
-                    lis =
-                        listHelp l start []
-                in
-                queue
-                    |> Expect.equal lis
+                ( queueOutput, revQueueOutput )
+                    |> Expect.equal ( listOutput, revListOutput )
             )
         ]
+
+
+type alias Output =
+    { finalContents : List Int
+    , finalContentsReversed : List Int
+    , youngestSnapShots : List (Maybe Int)
+    , oldestSnapShots : List (Maybe Int)
+    , finalLength : Int
+    }
+
+
+type Operation
+    = Enqueue Int
+    | Dequeue
+
+
+operationsFuzzer : Fuzzer ( List Int, List Operation )
+operationsFuzzer =
+    Fuzz.oneOfValues [ Enqueue 0, Dequeue ]
+        |> Fuzz.andThen
+            (\o ->
+                case o of
+                    Dequeue ->
+                        Fuzz.constant Dequeue
+
+                    Enqueue _ ->
+                        Fuzz.int
+                            |> Fuzz.map Enqueue
+            )
+        |> Fuzz.listOfLength 400
+        |> Fuzz.pair (Fuzz.list Fuzz.int)
+
+
+type alias IntermediateQueue =
+    { queue : Queue Int
+    , youngestSnapShots : List (Maybe Int)
+    , oldestSnapShots : List (Maybe Int)
+    }
+
+
+queueHelp : List Operation -> IntermediateQueue -> Output
+queueHelp operationsToBeDone soFar =
+    case operationsToBeDone of
+        [] ->
+            { finalContents = Queue.toListLIFO soFar.queue
+            , finalContentsReversed = Queue.toListFIFO soFar.queue
+            , youngestSnapShots = soFar.youngestSnapShots
+            , oldestSnapShots = soFar.oldestSnapShots
+            , finalLength = Queue.length soFar.queue
+            }
+
+        operation :: rest ->
+            let
+                newQueue =
+                    case operation of
+                        Dequeue ->
+                            Queue.dequeue soFar.queue
+
+                        Enqueue x ->
+                            Queue.enqueue x soFar.queue
+            in
+            queueHelp rest
+                { queue = newQueue
+                , youngestSnapShots =
+                    Queue.rear newQueue :: soFar.youngestSnapShots
+                , oldestSnapShots =
+                    Queue.head newQueue :: soFar.oldestSnapShots
+                }
+
+
+type alias IntermediateList =
+    { queue : List Int
+    , youngestSnapShots : List (Maybe Int)
+    , oldestSnapShots : List (Maybe Int)
+    }
+
+
+listHelp : List Operation -> IntermediateList -> Output
+listHelp operationsToBeDone soFar =
+    case operationsToBeDone of
+        [] ->
+            { finalContents = soFar.queue
+            , finalContentsReversed = List.reverse soFar.queue
+            , youngestSnapShots = soFar.youngestSnapShots
+            , oldestSnapShots = soFar.oldestSnapShots
+            , finalLength = List.length soFar.queue
+            }
+
+        operation :: rest ->
+            let
+                newQueue =
+                    case operation of
+                        Dequeue ->
+                            soFar.queue
+                                |> List.Extra.init
+                                |> Maybe.withDefault []
+
+                        Enqueue x ->
+                            x :: soFar.queue
+            in
+            listHelp rest
+                { queue = newQueue
+                , youngestSnapShots =
+                    List.head newQueue :: soFar.youngestSnapShots
+                , oldestSnapShots =
+                    List.Extra.last newQueue :: soFar.oldestSnapShots
+                }
